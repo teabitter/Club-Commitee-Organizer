3723ed75ba41f0351bd29238a82b125cdb71cd8d
studentam <studentam@DESKTOP-00U67KE>
2024-04-26 08:48:15 -0400

    Add project files.

---
+++ Club Commitee Organizer/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Club_Commitee_Organizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxAllMembers = new System.Windows.Forms.ListBox();
+            this.listBoxCurrentMembers = new System.Windows.Forms.ListBox();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(160, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Committee";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(335, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "General Member List";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(417, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Current Members";
+            // 
+            // listBoxAllMembers
+            // 
+            this.listBoxAllMembers.FormattingEnabled = true;
+            this.listBoxAllMembers.Location = new System.Drawing.Point(85, 146);
+            this.listBoxAllMembers.Name = "listBoxAllMembers";
+            this.listBoxAllMembers.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxAllMembers.Size = new System.Drawing.Size(119, 121);
+            this.listBoxAllMembers.TabIndex = 4;
+            // 
+            // listBoxCurrentMembers
+            // 
+            this.listBoxCurrentMembers.FormattingEnabled = true;
+            this.listBoxCurrentMembers.Location = new System.Drawing.Point(421, 146);
+            this.listBoxCurrentMembers.Name = "listBoxCurrentMembers";
+            this.listBoxCurrentMembers.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxCurrentMembers.Size = new System.Drawing.Size(119, 121);
+            this.listBoxCurrentMembers.TabIndex = 5;
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(275, 175);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(75, 23);
+            this.buttonRight.TabIndex = 6;
+            this.buttonRight.Text = "-->";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(275, 217);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeft.TabIndex = 7;
+            this.buttonLeft.Text = "<--";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(262, 277);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(101, 32);
+            this.buttonQuit.TabIndex = 8;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 321);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.listBoxCurrentMembers);
+            this.Controls.Add(this.listBoxAllMembers);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxAllMembers;
+        private System.Windows.Forms.ListBox listBoxCurrentMembers;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonQuit;
+    }
+}
+
---
+++ Club Commitee Organizer/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Club_Commitee_Organizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(new string[]
+            {
+                "Activities",
+                "Community Services",
+                "Executive",
+                "Membership",
+                "Programming Team",
+                "Scholarship",
+                "Sports",
+                "Travel",
+                "Volunteer Tutoring"
+            });
+
+            List<string> generalMembers = new List<string>
+        {
+            "Adams, Ben",
+            "Baker, Sam",
+            "Chong, Anne",
+            "Davis, Sandra",
+            "Easterlin, John",
+            "Fernandez, Jose",
+            "Fox, Barbara",
+            "Gomez, Ignacio"
+        };
+            listBoxAllMembers.Items.AddRange(generalMembers.ToArray());
+
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            foreach(var selectedMember in listBoxAllMembers.SelectedItems)
+            {
+                if(listBoxAllMembers.Items.Contains(selectedMember))
+                {
+                    listBoxCurrentMembers.Items.Add(selectedMember);
+                }
+            }
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            var selectedMembers = new List<object>(listBoxCurrentMembers.SelectedItems.Cast<object>());
+
+            foreach (var selectedMember in selectedMembers)
+                {
+                listBoxCurrentMembers.Items.Remove(selectedMember);
+                }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a committee first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
